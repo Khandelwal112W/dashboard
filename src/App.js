b17ac73fcdceb48b16ea83974e96abba6adf6e8d
Khandelwal112W <162304599+Khandelwal112W@users.noreply.github.com>
2024-03-05 22:06:15 +0530

    new app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// App.js
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
@@ -7,26 +6,37 @@ import Pages from "./components/Pages";
 import Categories from "./components/Categories";
 import Users from "./components/Users";
 import SideNavbar from "./components/SideNavbar";
+import Login from "./components/Login";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  // Function to handle successful login
+  const handleLogin = () => {
+    setIsLogin(true);
+  };
+
   return (
-    <Router>
-      <Navbar />
-      <div
-        className="flex px-10 mt-10
-       gap-10"
-      >
-        <SideNavbar />
-        <div className="flex-1">
-          <Routes>
-            <Route exact path="/" element={<Dashboard />} />
-            <Route path="/pages" element={<Pages />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/users" element={<Users />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <>
+      {!isLogin ? (
+        <Login onLogin={handleLogin} />
+      ) : (
+        <Router>
+          <Navbar />
+          <div className="flex px-10 mt-10 gap-10">
+            <SideNavbar />
+            <div className="flex-1">
+              <Routes>
+                <Route exact path="/" element={<Dashboard />} />
+                <Route path="/pages" element={<Pages />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/users" element={<Users />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      )}
+    </>
   );
 }
 
